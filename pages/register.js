5df78b0fa8bd788cce550e42efe56ea441040e49
meraj-sheikh <meraj.sheikh.1997@gmail.com>
2022-08-21 17:34:46 +0530

    Configured Tailwind CSS in project

---
+++ pages/register.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Register() {
+  return (
+    <div>
+        <Head>
+            <title>Register</title>
+        </Head>
+        
+      <h1>Register</h1>
+    </div>
+  )
+}
